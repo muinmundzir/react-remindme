5eaf91a2ea0ac76f3b91799c119ed30ae24f7c53
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2022-01-05 14:49:55 +0700

    edit: import components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Advantages from './components/Advantages'
+import UserPaging from './components/UserPaging'
+import Testimonial from './components/Testimonial'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <Advantages />
+      <UserPaging />
+      <Testimonial />
+      <Footer />
+    </>
+  )
 }
 
-export default App;
+export default App
